3e2fd07f327ecbfd64a6a6395aa4454ff5d50134
Omar Hamdan <93857681+OmarHamdann@users.noreply.github.com>
2022-06-27 14:03:44 +0000

    done

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+"use strict";
+
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+// require and use "multer"...
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+var upload = multer({ dest: "uploads/" });
+app.post("/api/fileanalyse", upload.single("upfile"), function(req, res, next) {
+  var upfile = req.file;
+  if (typeof upfile === "undefined") res.json({ error: "file not uploaded" });
+  return res.json({
+    name: upfile.originalname,
+    type: upfile.mimetype,
+    size: upfile.size
+  });
+});
 
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Node.js listening ...");
 });
